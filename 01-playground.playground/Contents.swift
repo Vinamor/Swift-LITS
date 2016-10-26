e7207d3df1db0eed350a4eb7bb64b4c0acdc8603
Vinamor <romanivost@gmail.com>
2016-10-27 01:15:42 +0300

    замінив одиночні теги на множину
    поддавав optional binding який не працює так як мав би
    постараюсь виправити це

--- 01-playground.playground/Contents.swift
+++ 01-playground.playground/Contents.swift
@@ -1,4 +1,6 @@
+
 import UIKit
+import Foundation
 
 
 class NotebookRecord {
@@ -6,32 +8,42 @@ class NotebookRecord {
     var name: String?
     var text: String?
     var tags: Set<String>?
+    
+
     /*
-    var tag1: String?
-    var tag2: String?
-    var tag3: String?
-    */
+    init(date: Date) {
+        self.date = date
+    }
+ */
     
     init(date: Date, name: String?, text: String?, tags: Set<String>?) {
         self.date = date
         self.name = name
         self.text = text
         self.tags = tags
-        
-        /*
-        self.tag1 = tag1
-        self.tag2 = tag2
-        self.tag3 = tag3
-        */
     }
     
+    
     func fullDescriotion() -> String {
-        return "\(date)  \n  \(name!)  \n   \(tags!)  \n  \(text!)"
+        if let name = self.name, let tags = self.tags, let text = self.text
+        {
+        return "\(date) \n \(name) \n \(tags) \n  \(text)"
+        }
+        else{
+            return "\(date)"
+        }
     }
 }
 
+//так чомусь не працює ?? не розумію в чому справа
+//let date = Calendar.date(from: DateComponents(year: 2016, month: 10, day: 27))
+
 let date = Date()
 
-var obj = NotebookRecord(date: date, name: "Monday", text: "I'm very tired", tags: ["xc", "xx", "ss"])
+var obj1 = NotebookRecord(date: date, name: "Wednesday", text: "But, unfortunately, I'm very tired", tags: ["#newstart", "#fun", "#letsdosomething"] )
+var obj2 = NotebookRecord(date: date, name: nil, text: nil, tags: nil)
+//var obj3 = NotebookRecord(date: date)
 
-obj.fullDescriotion()
+
+obj1.fullDescriotion()
+obj2.fullDescriotion()
