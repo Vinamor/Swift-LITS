bb12c281329746d42b0be702ce8e0d930cace441
Vinamor <romanivost@gmail.com>
2016-10-27 15:19:23 +0300

    commit -m "01-playgrounds"

--- 01-playground.playground/Contents.swift
+++ 01-playground.playground/Contents.swift
@@ -3,14 +3,15 @@ import UIKit
 import Foundation
 
 
+
 class NotebookRecord {
     let date: Date
     var name: String?
     var text: String?
     var tags: Set<String>?
     
-    init(date: Date, name: String? = nil, text: String? = nil, tags: Set<String>? = nil) {
-        self.date = date
+    init(name: String? = nil, text: String? = nil, tags: Set<String>? = nil) {
+        self.date = Date()
         self.name = name
         self.text = text
         self.tags = tags
@@ -19,27 +20,30 @@ class NotebookRecord {
     
     func fullDescriotion() -> String {
         
-         let name = self.name ?? ""
-         let tags = self.tags ?? Set<String>()
-         let text = self.text  ?? ""
+        let name = self.name ?? ""
+        let tag = self.tags ?? Set<String>()
+        let text = self.text ?? ""
         
-         return "\(date) \n \(name) \n \(tags) \n  \(text)"
+        return "\(date) \n \(name) \n \(tag) \n  \(text)"
     }
 }
 
-//так чомусь не працює ?? не розумію в чому справа
-//let date = Calendar.date(from: DateComponents(year: 2016, month: 10, day: 27))
 
-//let d = Calendar.date(from: DateComponents(year: 2016)
+//public func date(from components: DateComponents) -> Date?
+
+
+//так чомусь не працює ?? не розумію в чому справа
+let date1 = Calendar.date(from: DateComponents(year: 2016, month: 10, day: 27))
+
 let date = Date()
 
-let obj1 = NotebookRecord(date: date, name: "Wednesday", text: "But, unfortunately, I'm very tired", tags: ["#newstart", "#fun", "#letsdosomething"] )
-let obj2 = NotebookRecord(date: date)
-let obj3 = NotebookRecord(date: date, name: "Thursday")
-let obj4 = NotebookRecord(date: date, text: "Here's no other fields except date")
-let obj5 = NotebookRecord(date: date, name: "Friday", tags: ["#horrayFriday", "#therestisclose"] )
-let obj6 = NotebookRecord(date: date, tags: ["#fun", "#enjoy your day"])
-let obj7 = NotebookRecord(date: date, text: "Hi, everybody")
+let obj1 = NotebookRecord(name: "Wednesday", text: "But, unfortunately, I'm very tired", tags: ["#newstart", "#fun", "#letsdosomething"] )
+let obj2 = NotebookRecord()
+let obj3 = NotebookRecord(name: "Thursday")
+let obj4 = NotebookRecord(text: "Here's no other fields except date")
+let obj5 = NotebookRecord(name: "Friday", tags: ["#horrayFriday", "#therestisclose"] )
+let obj6 = NotebookRecord(tags: ["#fun", "#enjoy your day"])
+let obj7 = NotebookRecord(text: "Hi, everybody")
 
 
 obj1.fullDescriotion()
